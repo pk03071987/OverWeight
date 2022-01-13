42f9049acc73ad45cb28cf51827de0107eb9cbc2
kumar3787 <pramodkumar1@virtualemployee.com>
2022-01-13 11:15:12 +0530

    background environment work on 12.1.2022

--- Assets/Scripts/playersController.cs
+++ Assets/Scripts/playersController.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class playersController : MonoBehaviour
 {
     float x;
-    public float speed, speedForward;
+    public float speed, downwardSpeed;
+    public float forwardSpeed = 10f;
     public bool gamerun, winlevel;
     public bool leftw, rightw;
     Vector3 a, currentspeed;
@@ -48,11 +49,6 @@ public class playersController : MonoBehaviour
     {
         if (UiManager.isPause)
             return;
-        //if(transform.childCount-1>0 && transform.childCount-1> int.Parse(gamemanager.instance.playerCounterGameplay.text))
-        //{
-        //    gamemanager.instance.playerCounterGameplay.text = (transform.childCount-1).ToString();
-        //    gamemanager.instance.plauerCounterWin.text = (transform.childCount-1).ToString();
-        //}
 
         lvlbar.fillAmount = (transform.position.z - startZ) / levellong;
         
@@ -63,29 +59,17 @@ public class playersController : MonoBehaviour
 
         if (transform.position.z - endlvl.transform.position.z >= 0f)
         {
-            //if (!countAnim)
-            //{
-            //    gamemanager.instance.playerNum.text = transform.childCount.ToString();
-            //    gamemanager.instance.enemyNum.text = enemCtr.gameObject.transform.childCount.ToString();
-            //    gamemanager.instance.enemVsPlayer.SetActive(true);
-            //}
+          
         }
 
         if (Input.GetMouseButtonDown(0) && !gamerun)
         {
-            //gamerun = true;
-            //StartCoroutine(alertTogoal());
-            //transform.GetChild(0).GetComponent<Animator>().SetTrigger("run");
-            //transform.GetChild(1).GetComponent<Animator>().SetTrigger("walk");
-            //gamemanager.instance.startpanel.SetActive(false);
-            //gamemanager.instance.gamePlaypanel.SetActive(true);
+          
         }
         if (gamerun && !winlevel && transform.childCount > 0)
         {
             timer += Time.deltaTime;
-            Debug.Log("timer " + timer);
-            //  transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + speedForward * Time.deltaTime);
-            transform.position = new Vector3(transform.position.x, transform.position.y - speedForward * Time.deltaTime, transform.position.z);
+            transform.position = new Vector3(transform.position.x, transform.position.y - downwardSpeed * Time.deltaTime, transform.position.z+(forwardSpeed*Time.deltaTime));
         }
 
         if (Input.GetMouseButtonDown(0))
@@ -98,8 +82,6 @@ public class playersController : MonoBehaviour
                 x = hit.point.x;
                 pressPos = hit.point;
             }
-
-
         }
 
         if (Input.GetMouseButtonUp(0))
@@ -110,39 +92,21 @@ public class playersController : MonoBehaviour
         if (Input.GetMouseButton(0) && transform.childCount > 0 && !winlevel)
         {
 
-
-
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit, 100))
             {
                 direction = hit.point - pressPos;
-
-                //Debug.Log("without//// " + Input.mousePosition.magnitude);
-                //if (direction.magnitude < 2)
-                //{
-                //    return;
-                //}
-
-
                 if (hit.point.x > x && !rightw)
                 {
                     pressPos = Input.mousePosition;
-
-
                     float vx = hit.point.x - x;
                     x = hit.point.x;
-
-
                     a = transform.position;
                     a.x += vx * 1.5f;
-                    //a.x += direction.normalized.magnitude * (speed/2)*Time.deltaTime;
-                    //transform.position = Vector3.Lerp(transform.position, a, speed);
-                    //transform.Translate(Vector3.right * speed * Time.deltaTime);
                     transform.position = Vector3.SmoothDamp(transform.position, a, ref currentspeed, Time.deltaTime, speed * direction.normalized.magnitude);
-                    //transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
-
                 }
+
                 if (hit.point.x < x && !leftw)
                 {
                     pressPos = Input.mousePosition;
@@ -150,42 +114,9 @@ public class playersController : MonoBehaviour
                     x = hit.point.x;
                     a = transform.position;
                     a.x -= vx * 1.5f;
-                    //a.x -= direction.normalized.magnitude * (speed / 2) * Time.deltaTime;
-                    //transform.position = Vector3.Lerp(transform.position, a, speed);
-                    //transform.Translate(Vector3.right * -speed * Time.deltaTime);
                     transform.position = Vector3.SmoothDamp(transform.position, a, ref currentspeed, Time.deltaTime, speed * direction.normalized.magnitude);
-                    //transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
                 }
-
-
             }
-
-
-            //if (Input.mousePosition.x > x && !rightw)
-            //{
-            //    float vx = Input.mousePosition.x - x;
-            //    x = Input.mousePosition.x;
-            //    a = transform.position;
-            //    a.x += vx * Time.deltaTime;
-            //    //a.x += direction.normalized.magnitude * (speed/2)*Time.deltaTime;
-            //    //transform.position = Vector3.Lerp(transform.position, a, speed*Time.deltaTime);
-            //    transform.Translate(Vector3.right * speed * Time.deltaTime);
-            //    //transform.position = Vector3.SmoothDamp(transform.position, a,ref currentspeed, Time.deltaTime,speed,Time.deltaTime);
-            //    //transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
-
-            //}
-            //if (Input.mousePosition.x < x && !leftw)
-            //{
-            //    float vx = x - Input.mousePosition.x;
-            //    x = Input.mousePosition.x;
-            //    a = transform.position;
-            //    a.x -= vx * Time.deltaTime;
-            //    //a.x -= direction.normalized.magnitude * (speed / 2) * Time.deltaTime;
-            //    //transform.position = Vector3.Lerp(transform.position, a, speed*Time.deltaTime);
-            //    transform.Translate(Vector3.right * -speed * Time.deltaTime);
-            //    //transform.position = Vector3.SmoothDamp(transform.position, a, ref currentspeed,Time.deltaTime, speed,Time.deltaTime);
-            //    //transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
-            //}
         }
 
 
@@ -193,7 +124,6 @@ public class playersController : MonoBehaviour
         if (winlevel && !FrOnce)
         {
             FrOnce = true;
-            //StartCoroutine(playerlastfight());
         }
 
         if (transform.childCount == 0)
@@ -212,14 +142,12 @@ public class playersController : MonoBehaviour
         {
             if (i < enemCtr.gameObject.transform.childCount)
             {
-                Debug.Log(i + " // " + enemCtr.gameObject.transform.childCount);
                 GameObject gm = enemCtr.gameObject.transform.GetChild(i).gameObject;
                 transform.GetChild(i).gameObject.GetComponent<soliderCtr>().onelastshoot(gm);
 
             }
             else if (i >= enemCtr.gameObject.transform.childCount)
             {
-                Debug.Log("numb " + i);
                 GameObject gm = enemCtr.gameObject.transform.GetChild(Random.Range(0, enemCtr.gameObject.transform.childCount - 1)).gameObject;
                 transform.GetChild(i).gameObject.GetComponent<soliderCtr>().onelastshoot(gm);
 
